b8a52f1d16f49e4a8e03995f506d60c9fad54390
Devi Sandeep Endluri <sandeep0138@gmail.com>
2020-05-10 00:35:32 -0500

    Fix remaining codacy issues.

--- js/resume.js
+++ js/resume.js
@@ -3,7 +3,7 @@
 
   // Smooth scrolling using jQuery easing
   $("a.js-scroll-trigger[href*=\"#\"]:not([href=\"#\"])").click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    if (location.pathname.replace(/^\/\/, "") === this.pathname.replace(/^\/\/, "") && location.hostname === this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       if (target.length) {
@@ -13,7 +13,7 @@
         return false;
       }
     }
-  });
+  })));
 
   // Closes responsive menu when a scroll trigger link is clicked
   $(".js-scroll-trigger").click(function() {
